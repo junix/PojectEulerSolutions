3cc00f5f6c5d7f892ef77a8b3a9cfcee872651be
junix <juns1984@qq.com>
2015-03-22 20:58:16 +0800

    修改：     LongestSubstringWithoutRepeatingCharacters.hs

--- LeetCode/LongestSubstringWithoutRepeatingCharacters.hs
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters.hs
@@ -5,22 +5,16 @@
     which the length is 3. For "bbbbb" the longest substring 
     is "b", with the length of 1.
 -}
-
 import Control.Monad.State
 
 step c = state $ \(maxs,tails) -> 
-    let 
-      ntails  = c:(takeWhile (/=c) tails)
-      maxLen  = length maxs
-      tailLen = length ntails
-      nmaxs   = if maxLen < tailLen then ntails else maxs
-    in 
-      (length nmaxs,(nmaxs,ntails))
+    let ntails = c:(takeWhile (/=c) tails)
+        mlen   = length maxs
+        tlen   = length ntails
+        nmaxs  = if mlen < tlen then ntails else maxs
+    in  (max mlen tlen,(nmaxs,ntails))
 
-eat ""     = do return 0
-eat (x:xs) = do 
-  v0 <-step x
-  v1 <-eat xs
-  return $ max v0 v1
+eat ""     = do { return 0 }
+eat (x:xs) = do { v0 <-step x; v1 <-eat xs; return $ max v0 v1 }
 
 calc str = evalState (eat str) ("","")

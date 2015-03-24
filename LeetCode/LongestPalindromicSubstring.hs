9f8b563b37c6d39d1802e93abfbe15e0a0f511dc
junix <juns1984@qq.com>
2015-03-24 10:30:27 +0800

    修改：     LongestPalindromicSubstring.hs

--- LeetCode/LongestPalindromicSubstring.hs
+++ LeetCode/LongestPalindromicSubstring.hs
@@ -4,18 +4,17 @@
  and there exists one unique longest palindromic substring.
 -}
 
-isPali []  = True
-isPali [x] = True
-isPali (x:xs) = let t   = last xs
-                    mid = init xs
-                in  if x /= t then False else isPali mid 
+isPali xs = xs == reverse xs
 
-merge (maxs,acc) x = if isPali (x:acc) then (x:acc,x:acc) else (maxs,x:acc)
-newPali s = fst $ foldl merge ("","") s
+merge (maxs,acc) x = let acc'  = x:acc 
+                         maxs' = if isPali acc' then acc' else maxs
+                     in (maxs',acc')
+
+tailPali s = fst $ foldl merge ("","") s
 
 pali []       = []
 pali s@(x:xs) = 
-  let npali = newPali s
+  let npali = tailPali s
       tpali = pali xs
   in  if length npali > length tpali then 
          npali 

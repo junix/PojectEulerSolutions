81626fdc1501704438c0a8c0d2d5f938d86f32ec
junix <juns1984@qq.com>
2015-03-26 01:17:59 +0800

    新文件：   MergeIntervals.hs

---
+++ LeetCode/MergeIntervals.hs
@@ -0,0 +1,20 @@
+{-
+  Given a collection of intervals, merge all overlapping intervals.
+ 
+  For example,
+  Given (1,3),(2,6),(8,10),(15,18),
+  return (1,6),(8,10),(15,18).
+-}
+
+isOverLap (x1,y1) (x2,y2) = not (y1 < x2 || y2 < x1)
+
+mergeOverLap (x1,y1) (x2,y2) = (min x1 x2,max y1 y2)
+
+merge' :: (Integer,Integer) -> [(Integer,Integer)] -> [(Integer,Integer)]
+merge' e [] = [e]
+merge' e (x:xs)
+  | isOverLap e x = merge' (mergeOverLap e x) xs
+  | otherwise     = x : merge' e xs
+
+calc = foldr merge' []
+

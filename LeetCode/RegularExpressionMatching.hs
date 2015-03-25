1f0d17d3c6262912f34d42d91a07ca2a004a2ea0
junix <juns1984@qq.com>
2015-03-25 21:33:00 +0800

    修改：     RegularExpressionMatching.hs

--- LeetCode/RegularExpressionMatching.hs
+++ LeetCode/RegularExpressionMatching.hs
@@ -25,18 +25,18 @@ split ([],  rem) = [rem]
 split (x:xs,rem) = rem : split (xs,x:rem)
 
 advance :: MatchState -> [MatchState]
-advance ("",         ""     ) = [("","")]
-advance ("",         _      ) = []
-advance (".",        [_]    ) = [("","")]
-advance (".",        _      ) = []
-advance ([p],        [t]    ) = [("","") | p == t]
-advance ([_],        _      ) = []
-advance ('.':'*':xs, txt    ) = map (\r -> (xs,r)).split $ (reverse txt,[])
-advance ('.':xs,     []     ) = []
-advance ('.':pxs,    _:txs  ) = [(pxs,txs)]
-advance (c:'*':xs,   txt    ) = map (\r -> (xs,r)).split.span (==c) $ txt 
-advance (_:_,        []     ) = []
-advance (p:pxs,      t:txs  ) = [(pxs,txs) | p == t]
+advance ("",         ""   ) = [("","")]
+advance ("",         _    ) = []
+advance (".",        [_]  ) = [("","")]
+advance (".",        _    ) = []
+advance ([p],        [t]  ) = [("","") | p == t]
+advance ([_],        _    ) = []
+advance ('.':'*':xs, txt  ) = map (\r -> (xs,r)).split $ (reverse txt,[])
+advance ('.':xs,     []   ) = []
+advance ('.':pxs,    _:txs) = [(pxs,txs)]
+advance (c:'*':xs,   txt  ) = map (\r -> (xs,r)).split.span (==c) $ txt 
+advance (_:_,        []   ) = []
+advance (p:pxs,      t:txs) = [(pxs,txs) | p == t]
 
 match [] = False
 match xs = ("","") `elem` xs || match $ concatMap advance xs

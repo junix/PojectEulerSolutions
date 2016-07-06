cf9a92205307cfe4d9de884ce8c7e4fa532e6c16
wanglijun <juns1984@qq.com>
2016-07-06 20:45:26 +0800

    fix

--- src/RWH/Ch04/SplitLines.hs
+++ src/RWH/Ch04/SplitLines.hs
@@ -1,2 +1,31 @@
 module RWH.Ch04.SplitLines where
 
+splitLines :: String -> [String]
+splitLines =
+    go []
+  where go :: [String] -> String -> [String]
+        go xss []        = reverse xss
+        go xss ('\n':xs) = go xss xs
+        go xss ('\r':xs) = go xss xs
+        go xss rx@(x:xs) = go (line:xss) remain
+          where (line, remain) = break isLineTerm rx
+
+isLineTerm :: Char -> Bool
+isLineTerm x = x == '\n' || x == '\r'
+
+splitLines' :: String -> [String]
+splitLines' xs =
+  case dropWhile isLineTerm xs of
+    []  -> []
+    xs' -> let (l,r) = break isLineTerm xs'
+           in l:splitLines r
+
+splitLines'' :: String -> [String]
+splitLines'' s = snd.step.foldr go ([], []) $ s
+  where
+    go x acc@(xs, xss)
+      | isLineTerm x = step acc
+      | otherwise    = (x:xs, xss)
+    step r@([], xss) = r
+    step (xs, xss) = ([], xs:xss)
+

4eba5b02acac3076384749de18be9df85239c493
wanglijun <juns1984@qq.com>
2016-07-08 18:27:54 +0800

    add solutions

--- src/RWH/Ch08/ElfMagic.hs
+++ src/RWH/Ch08/ElfMagic.hs
@@ -2,7 +2,6 @@ import qualified Data.ByteString.Lazy as L
 import qualified System.IO
 import qualified System.Environment
 
-
 hasElfMagic :: L.ByteString -> Bool
 hasElfMagic content = L.take 4 content == elfMagic
   where elfMagic = L.pack [0x7f, 0x45, 0x4c, 0x46]

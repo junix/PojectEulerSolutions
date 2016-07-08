4514498999aec684fad6beee7980b6842f109511
wanglijun <juns1984@qq.com>
2016-07-08 18:22:40 +0800

    add solutions

---
+++ src/RWH/Ch08/ElfMagic.hs
@@ -0,0 +1,18 @@
+import qualified Data.ByteString.Lazy as L
+import qualified System.IO
+import qualified System.Environment
+
+
+hasElfMagic :: L.ByteString -> Bool
+hasElfMagic content = L.take 4 content == elfMagic
+  where elfMagic = L.pack [0x7f, 0x45, 0x4c, 0x46]
+
+isElfFile :: FilePath -> IO ()
+isElfFile path = do
+    content <- L.readFile path
+    print $ hasElfMagic content
+
+main = do
+  paths <- System.Environment.getArgs
+  mapM isElfFile paths
+  return ()

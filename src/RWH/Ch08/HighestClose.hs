007e6faef4c79ac94e1975875d39021c530f9286
wanglijun <juns1984@qq.com>
2016-07-08 20:46:26 +0800

    add solutions

---
+++ src/RWH/Ch08/HighestClose.hs
@@ -0,0 +1,33 @@
+import qualified Data.ByteString.Lazy.Char8 as L
+import System.Environment
+
+main = do
+    files <- System.Environment.getArgs
+    if null files
+        then putStrLn "no data file"
+        else do
+            content <- L.readFile (head files)
+            let maxPrice = maximum . (Nothing:) . map closing . L.lines $ content
+            print maxPrice
+
+readPrice :: L.ByteString -> Maybe Int
+readPrice str =
+  case L.readInt str of
+    Nothing ->
+        Nothing
+    Just (dollars ,rest) ->
+        case L.readInt (L.tail rest) of
+            Nothing ->
+                Nothing
+            Just (cents, _) ->
+                Just (dollars * 100 + cents)
+
+readPrice' :: L.ByteString -> Maybe Int
+readPrice' str = do
+  (dollars, rest) <- L.readInt str
+  (cents, _) <- L.readInt (L.tail rest)
+  return (dollars * 100 + cents)
+
+closing :: L.ByteString -> Maybe Int
+closing = readPrice . (!!4) . L.split ','
+

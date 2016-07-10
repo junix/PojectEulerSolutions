b4c501ab2d89d8f5c9eea305c633ffb9fb1168c9
wanglijun <juns1984@qq.com>
2016-07-11 00:35:59 +0800

    add solutions

---
+++ src/Codewars/MergeChecker.hs
@@ -0,0 +1,24 @@
+module Codewars.MergeChecker where
+
+{-
+At a job interview, you are challenged to write an algorithm to check if a given string, s, can be formed from two other strings, part1 and part2.
+The restriction is that the characters in part1 and part2 are in the same order as in s.
+The interviewer gives you the following example and tells you to figure out the rest from the given test cases.
+For example:
+
+'codewars' is a merge from 'cdw' and 'oears':
+
+    s:  c o d e w a r s   = codewars
+part1:  c   d   w         = cdw
+part2:    o   e   a r s   = oears
+-}
+
+isMerge :: String -> String -> String -> Bool
+isMerge [] [] [] = True
+isMerge [] _ _   = False
+isMerge xs [] zs = xs == zs
+isMerge xs ys [] = xs == ys
+isMerge (x:xs) ys'@(y:ys) zs'@(z:zs)
+    | x == y     = isMerge xs ys zs' || if (x == z) then isMerge xs ys' zs else False
+    | x == z     = isMerge xs  ys' zs
+    | otherwise  = False

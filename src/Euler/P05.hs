6a6dcc13dceecc4fbe97355ce59901511e69c232
wanglijun <juns1984@qq.com>
2016-07-11 20:24:20 +0800

    add solutions

---
+++ src/Euler/P05.hs
@@ -0,0 +1,16 @@
+module Euler.P05 where
+
+{-
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+-}
+
+primes = filterPrime [2..]
+  where filterPrime (p:xs) =
+          p : filterPrime [x | x <- xs, x `mod` p /= 0]
+
+calc n = head . filter canDivs $ [ps, ps*2 ..]
+    where ps = product.takeWhile (<n) $ primes
+          canDivs x = all ((==0).(x `rem`)) [2..n]
+
+
---
+++ src/Euler/P06.hs
@@ -0,0 +1,20 @@
+module Euler.P06 where
+import Control.Monad
+{-
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+-}
+
+-- reader monad
+calc :: [Integer] -> Integer
+calc = do
+    x <- (^2) . sum
+    y <- sum . map (^2)
+    return (x-y)

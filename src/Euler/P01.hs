e5d61c23aab3540c15f365db58dfe9d3242e6808
wanglijun <juns1984@qq.com>
2016-07-11 16:40:40 +0800

    add state slolutions

---
+++ src/Euler/P01.hs
@@ -0,0 +1,14 @@
+module Euler.P01 where
+
+{-
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+-}
+
+compof35 n
+    | n `rem` 3 == 0 = True
+    | n `rem` 5 == 0 = True
+    | otherwise      = False
+
+seq35 = sum . filter compof35 $ [3..999]
+

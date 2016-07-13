8ef11f74f24a5c4cacf97e8ebd562182403bbb0e
wanglijun <juns1984@qq.com>
2016-07-13 12:53:35 +0800

    add  slolutions

--- src/Euler/P037.hs
+++ src/Euler/P037.hs
@@ -3,15 +3,8 @@ import Math.NumberTheory.Primes
 import Data.List
 import Data.Bits ((.&.))
 
-
 isTruncPrime :: String -> Bool
-isTruncPrime = all isPrime . lrseq
-
-lrseq :: String -> [Integer]
-lrseq xs = map read $ (mid . inits) xs ++ (mid . tails) xs
-    where mid []  = []
-          mid [x] = []
-          mid xs  = tail . reverse . tail $ xs
+isTruncPrime xs = all isPrime . map read $ (tail.inits) xs ++ (init . tails) xs
 
 euler37 = sum . map read . take 11 . filter isTruncPrime . map show . drop 4 $ primes
 

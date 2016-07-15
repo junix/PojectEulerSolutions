1fd9e440f9c49862fd49600c9ad3c932c526b9ee
wanglijun <juns1984@qq.com>
2016-07-15 15:27:14 +0800

    add  slolutions

--- src/Euler/P067.hs
+++ src/Euler/P067.hs
@@ -4,11 +4,11 @@ import System.IO
 getTri = do
     content <- readFile "p067_triangle.txt"
     let tri = [map read vec :: [Integer] |vec <- (map words. lines $ content)]
-    print $ euler.reverse $ tri
+    print $ euler tri
 
 calc :: [Integer] -> [[Integer]] -> Integer
 calc (p:_) ([c]:[]) = p+c
 calc ps  (cs:css) = calc (zipWith max ms (tail ms)) css
     where ms = zipWith (+) ps cs
 
-euler xss = calc (repeat 0) xss
+euler xss = calc (repeat 0) . reverse $ xss

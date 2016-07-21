8362ce984381b0ce62c3459792fe1ec14c9be383
wanglijun <juns1984@qq.com>
2016-07-21 22:27:07 +0800

    add  slolutions

---
+++ src/Euler/P054.hs
@@ -0,0 +1,111 @@
+module Euler.P054 where
+import Data.List (nub,sort,permutations)
+import qualified Data.Map.Strict as M
+import qualified Data.Tuple (swap)
+
+data CardType = H | C | S | D deriving (Eq, Show, Read, Ord, Enum)
+data Rule = HighCard Int
+          | Pair Int Int Int Int
+          | TwoPair Int Int
+          | ThreeKind Int Int Int
+          | Straight Int
+          | Flush
+          | FullHouse Int Int
+          | StraightFlush Int
+          | FourKind Int Int deriving (Eq,Ord,Show)
+
+data Card = Card CardType Int
+
+instance Eq (Card) where
+    Card _ v1 == Card _ v2 = v1 == v2
+
+instance Ord Card where
+    Card _ v1 `compare` Card _ v2 = v1 `compare` v2
+
+showv 10 = "T"
+showv 11 = "J"
+showv 12 = "Q"
+showv 13 = "K"
+showv 14 = "A"
+showv n = show n
+
+readv "T" = 10
+readv "J" = 11
+readv "Q" = 12
+readv "K" = 13
+readv "A" = 14
+readv s = read s :: Int
+
+instance Show Card where
+    show (Card t v) = showv v ++ show t
+
+cardType (Card t v) = t
+cardValue (Card t v) = v
+
+sameSuit :: [Card] -> Bool
+sameSuit = (==1).length . nub . map cardType
+
+cons :: [Card] -> Maybe Int
+cons cards = if isCons then Just . head $ values else Nothing
+    where values = sort . map cardValue $ cards
+          isCons = all (==(-1)). zipWith (-) values $ (tail values)
+
+parse [v,t] = Card (read [t]) (readv [v])
+
+main = do
+    c <- readFile "p054_poker.txt"
+    let rounds = lines c
+    --let f = filter ((==GT).snd). map comp $ rounds
+    let f = map comp $ rounds
+    p f
+    print (length . filter ((==GT).snd) $ f)
+
+comp line = (vs,cmp)
+    where (a,b) = (splitAt 5) . map parse.words $ line
+          vs = ((a,select a), (b,select b))
+          cmp = select a `compare` select b
+
+p [] = return ()
+p (x:xs) = print x >> p xs
+
+straightFlush cards = if sameSuit cards
+                      then case c of
+                             Just v -> [StraightFlush v]
+                             Nothing -> []
+                      else []
+    where c = cons cards
+
+fourKind cards = case stats cards of
+                   [(4,x),(1,y)] -> [FourKind x y]
+                   _ -> []
+
+fullHouse cards = case stats cards of
+                    [(3,x),(2,y)] -> [FullHouse x y]
+                    _ -> []
+
+flush cards = if sameSuit cards then [Flush] else []
+
+straight cards = case c of
+                   Just v -> [Straight v]
+                   Nothing -> []
+    where c = cons cards
+
+threeKind cards = case stats cards of
+                     [(3,x),(1,y),(1,z)] -> [ThreeKind x y z]
+                     _ -> []
+
+twoPair cards = case stats cards of
+                  ((2,x):(2,y):_) -> [TwoPair x y]
+                  _  -> []
+
+pair cards = case  stats $ cards of
+               [(2,x),(1,y),(1,z),(1,w)] -> [Pair x y z w]
+               _ -> []
+
+stats cards = reverse . sort . map Data.Tuple.swap . M.toList $ stats
+    where values = map cardValue cards
+          stats = foldl (\m v -> M.insertWith (+) v 1 m) M.empty  values
+
+highCard cards = [HighCard . maximum . map cardValue $ cards]
+
+select cards = head . concatMap ($cards) $ [ straightFlush ,fourKind ,fullHouse ,flush ,straight ,threeKind ,twoPair ,pair ,highCard]

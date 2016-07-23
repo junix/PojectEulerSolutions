90b5f4cda83565de76993bc08252006c3c229035
wanglijun <juns1984@qq.com>
2016-07-23 20:05:31 +0800

    add  slolutions

--- src/Euler/P079.hs
+++ src/Euler/P079.hs
@@ -1,2 +1,20 @@
 module Euler.P079 where
+import Data.List (sort,nub)
+import qualified Data.Map as M
 
+stats [] = []
+stats (x:xs) = zip (repeat x) xs ++ stats xs
+
+topoOrder ms = go freqDict []
+    where orderSet = sort . nub . concatMap stats $ ms
+          freqDict = [(k, map snd . filter ((==k).fst) $ orderSet) | k <- (sort.nub.concat $ ms)]
+          go [] acc = acc
+          go xs acc = go t (delcs ++ acc)
+            where delcs = sort . map fst . filter (null.snd) $ xs
+                  t = map (del delcs) . filter (not.null.snd) $ xs
+
+del cs (c,xs) = (c,filter (not.(`elem` cs)) xs)
+
+main = do
+    ms <- lines <$> readFile "./p079_keylog.txt"
+    print $ topoOrder ms

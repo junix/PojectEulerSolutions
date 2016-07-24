8665639fd8fa4eaede2486fb33494bc8702d3859
wanglijun <juns1984@qq.com>
2016-07-25 00:54:24 +0800

    add  slolutions

--- src/Euler/P075.hs
+++ src/Euler/P075.hs
@@ -1,4 +1,18 @@
 module Euler.P075 where
+import Math.NumberTheory.Primes
+import Data.List (group, sort, nub)
 
-calcRightTriInt n = length [(x,y,z) | x <- [1..n], y <- [x..(n-x)],let z = (n-x-y), z > y, x^2+y^2==z^2]
+limit = 1500000
+maxm = truncate . sqrt . fromInteger $ (limit `quot` 2)
 
+genSet :: [Integer]
+genSet = concat [ gen l l | m <- [2..maxm]
+                          , n <- [1..m-1]
+                          , gcd m n == 1
+                          , odd (m+n)
+                          , let l = 2*m*(m + n) ]
+  where gen l c
+          | c > limit = []
+          | otherwise = c : gen l (l+c)
+
+euler = length . filter ((==1).length) . group . sort $ genSet

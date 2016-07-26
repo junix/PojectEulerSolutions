d382aa3b1a445b26bdabc004f4a9f41f2bb18d4b
wanglijun <juns1984@qq.com>
2016-07-26 15:28:52 +0800

    add Timus solutions

--- src/Euler/P113.hs
+++ src/Euler/P113.hs
@@ -7,21 +7,21 @@ data Direction = Desc | Inc deriving (Show,Eq,Ord)
 type D = M.Map (Char, Integer, Direction) Integer
 
 initDict :: D
-initDict = M.fromList . concatMap (\x -> [((x,1,Inc), 1), ((x,1,Desc),1)]) $ ['0'..'9']
+initDict = M.fromList . concat $ [[((x,1,Inc), 1), ((x,1,Desc),1)] | x <- ['0'..'9']]
 
 update :: D -> Integer -> D
 update dict k = foldr (rupdate k) dict ['0'..'9']
 
 rupdate k ch d0 = d2
   where  ppos = k - 1
-         incc = sum . map (d0 M.!) . map (\x -> (x,ppos, Inc)) $ ['0'..ch]
+         incc = sum [d0 M.! (x,ppos, Inc) | x <- ['0'..ch]]
          d1   = M.insert (ch, k, Inc) incc d0
-         desc = sum . map (d1 M.!) . map (\x -> (x,ppos,Desc)) $ [ch..'9']
+         desc = sum [d1 M.! (x,ppos,Desc) | x <- [ch..'9']]
          d2   = M.insert (ch, k, Desc) desc d1
 
 pseq n = foldl' update initDict [2..n]
 
-allNonLeapCnt n = sum . map exactNonLeapCnt $ [1..n]
+allNonLeapCnt n = sum [countOnKDigits k | k <- [1..n]]
     where d = pseq n
-          exactNonLeapCnt k = (sum . map (d M.!) . map (\x -> (x,k,Inc))  $ ['1'..'9']) +
-                              (sum . map (d M.!) . map (\x -> (x,k,Desc)) $ ['1'..'9']) - 9
+          countOnKDigits k = sum [d M.! (x,k,Inc) | x <- ['1'..'9']] +
+                             sum [d M.! (x,k,Desc)| x <- ['1'..'9']] - 9

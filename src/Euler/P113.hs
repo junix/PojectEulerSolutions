a7481d1d294bf4a5fd8c900e66bb9eb9fa95954b
wanglijun <juns1984@qq.com>
2016-07-26 16:50:57 +0800

    add Timus solutions

--- src/Euler/P113.hs
+++ src/Euler/P113.hs
@@ -1,5 +1,6 @@
 module Euler.P113 where
 
+import Data.Map.Strict ((!), insert, fromList, toList)
 import qualified Data.Map.Strict as M
 import Data.List (foldl')
 
@@ -7,22 +8,20 @@ data Direction = Desc | Inc deriving (Show,Eq,Ord)
 type D = M.Map (Char, Integer, Direction) Integer
 
 initDict :: D
-initDict = M.fromList . concat $ [[((x,1,Inc), 1), ((x,1,Desc),1)] | x <- ['0'..'9']]
+initDict = fromList . concat $ [[((x,1,Inc), 1), ((x,1,Desc),1)] | x <- ['0'..'9']]
 
 update :: D -> Integer -> D
 update dict k = foldr (rupdate k) dict ['0'..'9']
 
 rupdate k ch d0 = d2
   where  ppos = k - 1
-         incc = sum [d0 M.! (x,ppos, Inc) | x <- ['0'..ch]]
-         d1   = M.insert (ch, k, Inc) incc d0
-         desc = sum [d1 M.! (x,ppos,Desc) | x <- [ch..'9']]
-         d2   = M.insert (ch, k, Desc) desc d1
+         incc = sum [d0 ! (x,ppos, Inc) | x <- ['0'..ch]]
+         d1   = insert (ch, k, Inc) incc d0
+         desc = sum [d1 ! (x,ppos,Desc) | x <- [ch..'9']]
+         d2   = insert (ch, k, Desc) desc d1
 
 pseq n = foldl' update initDict [2..n]
 
 allNonLeapCnt n = sum [countOnKDigits k | k <- [1..n]]
-    where d = pseq n
-          countOnKDigits k = incCnt + decCnt - 9
-            where incCnt = sum [d M.! (x,k,Inc) | x <- ['1'..'9']]
-                  decCnt = sum [d M.! (x,k,Desc)| x <- ['1'..'9']]
+    where dict = pseq n
+          countOnKDigits k = sum [ dict ! (x,k,y) | x <- ['1'..'9'], y <- [Inc, Desc] ] - 9

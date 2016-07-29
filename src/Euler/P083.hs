44fb6a0eda1acb522d637e7d520fb4725adb4e34
wanglijun <juns1984@qq.com>
2016-07-29 10:34:02 +0800

    add Timus solutions

--- src/Euler/P083.hs
+++ src/Euler/P083.hs
@@ -10,26 +10,28 @@ test = concat $
     , [537 , 699 , 497 , 121 , 956]
     , [805 , 732 , 524 , 37  , 331]]
 
-initm xs = newListArray (0,l) xs' :: ST s (STArray s Integer (Integer,Integer))
-   where l = toInteger $ length xs - 1
-         xs' = map (\x -> (10^10,x)) xs
+initm xs = newListArray (0, lastPos) xs' :: ST s (STArray s Integer (Integer,Integer))
+   where lastPos = toInteger $ length xs - 1
+         xs'     = map (\x -> (10^10,x)) xs
 
 m2v l x y = y*l + x
 
-nei l x y = filter (\(a,b) -> iN a && iN b) [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
-    where iN v = v >= 0 && v < l
+nei l x y = filter validPos [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
+    where inR v = v >= 0 && v < l
+          validPos (a,b) = inR a && inR b
 
-initLT :: STArray s Integer (Integer,Integer) -> ST s (STArray s Integer (Integer,Integer))
-initLT m = do{ (a,b) <- readArray m 0
-               ; writeArray m 0 (b,b)
-               ; return m }
+initLT :: STArray s Integer (Integer,Integer)
+       -> ST s (STArray s Integer (Integer,Integer))
+initLT m = do
+    { (_, b) <- readArray m 0
+    ; writeArray m 0 (b, b)
+    ; return m }
 
 update :: Integer
        -> STArray s Integer (Integer,Integer)
        -> ST s (STArray s Integer (Integer,Integer))
 update l m = go m 0 0 False
-   where
-         go :: STArray s Integer (Integer,Integer)
+   where go :: STArray s Integer (Integer,Integer)
             -> Integer
             -> Integer
             -> Bool
@@ -39,11 +41,12 @@ update l m = go m 0 0 False
             | x >= l && b = go m 0 0 False
             | y >= l = go m (x+1) 0 b
             | otherwise = do
-                     maxv <- mapM (\(a,b) -> readArray m (m2v l a b)) $ nei l x y
-                     (c,len) <- readArray m (m2v l x y)
-                     let newv = minimum (c:map ((len+).fst) maxv)
-                     writeArray m (m2v l x y) (newv, len)
-                     go m x (y+1) (b || newv /= c)
+                { maxv <- mapM (\(a,b) -> readArray m (m2v l a b)) $ nei l x y
+                ; (c,len) <- readArray m (m2v l x y)
+                ; let newv = minimum (c:map ((len+).fst) maxv)
+                ; writeArray m (m2v l x y) (newv, len)
+                ; go m x (y+1) (b || newv /= c)
+                }
 
 euler xs = last es
     where len = truncate . sqrt . fromInteger . toInteger . length $ xs

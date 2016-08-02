0f7807c01ba75dcce370ac7fb8862209a7ba47d7
wanglijun <juns1984@qq.com>
2016-08-02 12:02:16 +0800

    add Timus solutions

--- src/SQL/Tree.hs
+++ src/SQL/Tree.hs
@@ -13,13 +13,28 @@ paths children level = ppaths + cpaths
           cpaths = deps children level*level
           ppaths = paths children plevel
 
-addBars xs =  ("|"++) . (++"|") . concat . intersperse "|" $ xs
+movs children level = deps children (level-1)
 
-stats f = map addBars $ (titles : delims : stats)
+addBars :: [String] -> String
+addBars =  ('|':) . (++"|") . concat . intersperse "|"
+
+bold xs = if length xs <= 5 then "`"++xs++"`" else xs
+
+boldFmtTab :: [[Integer]] -> [[Bool]]
+boldFmtTab xxs = [ map (<10^5) xs | xs <- xxs]
+
+boldTab :: [[Bool]] -> [[Integer]] -> [[String]]
+boldTab xxs yys = [ map render $ zip xs ys | (xs,ys) <- zip xxs yys]
+    where render (False, x) = show x
+          render (True,  x) = ('`':show x) ++ "`"
+
+fmt = boldFmtTab [ c : map (deps c) [1..10] | c <- [2,3,4,5,6,7,8,9,10,15,20,50,100]]
+
+stats f = map addBars $ (titles : delims : rstats)
     where titles = "sub deps" : map (("level "++).show) [1..10]
           delims = replicate 11 " :--: "
-          stats = [ show c : map (bold.show.f c) [1..10] | c <- [2,3,4,5,6,7,8,9,10,15,20,50,100]]
-          bold xs = if length xs <= 5 then "`"++xs++"`" else xs
+          stats  = [ c : map (f c) [1..10] | c <- [2,3,4,5,6,7,8,9,10,15,20,50,100]]
+          rstats = boldTab fmt stats
 
 main = do
     mapM_ putStrLn (stats deps)

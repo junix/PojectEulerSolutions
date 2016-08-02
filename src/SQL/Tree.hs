4af1bf2f4aa6956d1b86ae350710692483255e89
wanglijun <juns1984@qq.com>
2016-08-02 10:50:38 +0800

    add Timus solutions

--- src/SQL/Tree.hs
+++ src/SQL/Tree.hs
@@ -1,4 +1,5 @@
 module SQL.Tree where
+import Data.List (intersect,intersperse)
 
 deps _ 0 = 1
 deps children level = ancestors + parents * children
@@ -11,3 +12,15 @@ paths children level = ppaths + cpaths
     where plevel = level - 1
           cpaths = deps children level*level
           ppaths = paths children plevel
+
+addBars :: [String] -> String
+addBars xs =  ("|"++) . (++"|") . concat . intersperse "|" $ xs
+
+printDeps = map addBars $ (titles : delims : stats)
+    where titles = "sub deps" : map (("level "++).show) [1..10]
+          delims = replicate 11 " :--: "
+          stats = [ show c : map (bold.show.deps c) [1..10] | c <- [2,3,4,5,6,7,8,9,10,15,20,50,100]]
+          bold xs = if length xs <= 5 then "`"++xs++"`" else xs
+
+main = do
+    mapM_ putStrLn printDeps

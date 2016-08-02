ad2c48c1d8692c7f48bb97381603e5e61a620e56
wanglijun <juns1984@qq.com>
2016-08-03 00:17:56 +0800

    add Timus solutions

---
+++ src/Euler/P117.hs
@@ -0,0 +1,23 @@
+module Euler.P117 where
+
+import qualified Data.Map.Strict as M
+import Control.Monad.State
+
+type S = M.Map Integer Integer
+
+f :: Integer -> State S Integer
+f n
+  | n < 2  = state $ \s -> (1, M.insert n 1 s)
+  | otherwise = state $ \s ->
+            case M.lookup n s of
+                Just v ->
+                     (v, s)
+                Nothing ->
+                    let subc = filter (>=0) [n-1,n-2,n-3,n-4]
+                        (xs, s') = runState (mapM f subc) s
+                    in (sum xs, M.insert n (sum xs) s')
+
+calc n = fst $ runState (f n) M.empty
+
+euler = calc 50
+

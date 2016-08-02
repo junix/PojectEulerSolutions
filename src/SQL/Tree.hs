afe2786e7624e51e05d0d77b0d1e827fecf260ff
wanglijun <juns1984@qq.com>
2016-08-02 11:18:59 +0800

    add Timus solutions

--- src/SQL/Tree.hs
+++ src/SQL/Tree.hs
@@ -13,14 +13,15 @@ paths children level = ppaths + cpaths
           cpaths = deps children level*level
           ppaths = paths children plevel
 
-addBars :: [String] -> String
 addBars xs =  ("|"++) . (++"|") . concat . intersperse "|" $ xs
 
-printDeps = map addBars $ (titles : delims : stats)
+stats f = map addBars $ (titles : delims : stats)
     where titles = "sub deps" : map (("level "++).show) [1..10]
           delims = replicate 11 " :--: "
-          stats = [ show c : map (bold.show.deps c) [1..10] | c <- [2,3,4,5,6,7,8,9,10,15,20,50,100]]
+          stats = [ show c : map (bold.show.f c) [1..10] | c <- [2,3,4,5,6,7,8,9,10,15,20,50,100]]
           bold xs = if length xs <= 5 then "`"++xs++"`" else xs
 
 main = do
-    mapM_ putStrLn printDeps
+    mapM_ putStrLn (stats deps)
+    putStrLn ""
+    mapM_ putStrLn (stats paths)

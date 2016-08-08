f3de2b2eeeed45bf39e3a6ae973ae841a2fdf841
wanglijun <juns1984@qq.com>
2016-08-08 21:57:49 +0800

    add PE solution

--- src/Euler/P101.hs
+++ src/Euler/P101.hs
@@ -1,9 +1,18 @@
 module Euler.P101 where
-import Numeric.LinearAlgebra
-a = (4><3)
- [ 1, 2, 3
- , 4, 0, 5
- , 7, 7, 2
- , 3, 3, 1 ] :: Matrix Z
+import Data.List (delete)
+import Data.Ratio
 
+f x = sum . zipWith (*) coef . map (x^) $ [0..10]
+    where coef = cycle $ [1,-1]
 
+nseq :: [Integer]
+nseq = map f [1..11]
+
+fn :: Integer -> Integer -> Integer
+fn n m =  numerator . sum $ [ p x y m | (x,y) <- points ]
+    where points = zip [1..] . take (fromInteger n) $ nseq
+          p x y = \v -> y % (product [ x - xk |(xk,yk) <- rs]) *  (product [ v - xk | (xk,yk) <- rs] % 1)
+               where rs = delete (x,y) points
+
+euler :: Integer
+euler = sum [ fn n (n+1) | n <- [1..10]]

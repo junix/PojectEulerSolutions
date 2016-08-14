dcf56b4952716211582095d70f4cacbac1b91745
wanglijun <juns1984@qq.com>
2016-08-14 13:48:25 +0800

    fix

--- src/Euler/P122.hs
+++ src/Euler/P122.hs
@@ -1,22 +1,24 @@
 module Euler.P122 where
+import Data.List (nub)
+import qualified  Data.Map as M
+import qualified  Data.Set as S
 
-import qualified Data.Map.Lazy as M
+stepOf = update (c+1) [1..200] dict
+    where
+        (c, dict) = go 0 (S.fromList [(1,[1])]) (M.fromList [(1,0)])
+        go c xs dict
+            | M.size dict >= 199 = (c,dict)
+            | otherwise = go (c+1) xs' dict'
+                where xs' = S.fromList [ (e', (S.toList . S.fromList . (e':)) $ ss)
+                                       | (e,ss) <- S.toList xs
+                                       , s <- ss
+                                       , let e' = e + s
+                                       , e' <= 200
+                                       ]
+                      vs  = map fst . S.toList $ xs'
+                      dict' = update (c+1) vs dict
 
-initSteps n = M.fromList $ map (\x->(x,(x-1,[1]))) [1..n]
+update n ks dict = foldr (\k d -> M.insertWith ignore k n d) dict ks
+    where ignore = flip const
 
-iter m n = go m [1..n]
-    where go m [] = m
-          go m xs = foldl updateBy m xs
-
-          updateBy m x = foldl
-                (\d step ->
-                    if x+step > n
-                    then d
-                    else M.insertWith select (x+step) (c+1, c:steps) d) m (c:steps)
-              where (c,steps) = m M.! x
-
-          select (c1,steps1) (c2, steps2)
-              | c1 < c2   = (c1,steps1)
-              | c1 == c2  && length steps1 <= length steps2 = (c2,steps2)
-              | c1 == c2  && length steps1 > length steps2 = (c1,steps1)
-              | otherwise = (c2,steps2)
+euler = sum . M.elems $ stepOf

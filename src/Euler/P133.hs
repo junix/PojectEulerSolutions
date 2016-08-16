790ca097e79a3ca9fca13809fb5745bdc4126e00
wanglijun <juns1984@qq.com>
2016-08-16 23:34:27 +0800

    add mathematica solve

--- src/Euler/P133.hs
+++ src/Euler/P133.hs
@@ -15,24 +15,10 @@ goModPow m = go 1 (modPow 10 10 (9*m))
           go 100 _ = False
           go c r = go (c+1) ((r^10) `rem` (9*m))
 
-
-ps = S.fromList . takeWhile (<10^5) $ primes
-
-ps' = takeWhile (<10^5) $ primes
+ps' = takeWhile (<10^5) primes
 
 canDivide x n = modPow 10 (10^(n+1)) (x*9) == 1
 
 canMod x = any (canDivide x) [1..100]
 
-factorsOf n set = S.fromList . filter canDivide . S.toList $ set
-        where canDivide x = modPow 10 (10^(n+1)) (x*9) == 1
-
-factors = go 1 ps S.empty
-    where go n xs set = if set == set'
-                        then go (n+1) xs' set'
-                        else (length set',n) : go (n+1) xs' set'
-            where ds   = factorsOf n xs
-                  set' = S.union set ds
-                  xs'  = xs S.\\ ds
-
-e = filter (canMod)  ps'
+euler = sum $ ps' \\ filter goModPow ps'

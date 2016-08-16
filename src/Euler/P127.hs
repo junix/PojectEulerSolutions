b784aef9df0fe2298eb16ab2eb28779ec53d87ad
wanglijun <juns1984@qq.com>
2016-08-16 08:37:18 +0800

    add mathematica solve

--- src/Euler/P127.hs
+++ src/Euler/P127.hs
@@ -2,42 +2,45 @@ module Euler.P127 where
 
 import Data.List (intersect, (\\))
 import Math.NumberTheory.Primes
-import Data.Map.Lazy(fromList, empty,insert,lookup, (!))
+import Data.Map.Lazy(fromList, empty,insert,lookup, (!), toList)
 
 data Fact = Fact { fseq      :: [Integer]
                  , allOneExp :: Bool
                  , rad       :: Integer
+                 , v         :: Integer
                  } deriving Show
 
 factors :: Integer -> Fact
-factors n = Fact { fseq = map fst f
-                 , allOneExp = (maximum . (0:) .map snd $ f) == 1
-                 , rad = product . map fst $ f
+factors n = Fact { fseq      = map fst fs
+                 , allOneExp = (maximum . (0:) . map snd $ fs) == 1
+                 , rad       = product . map fst $ fs
+                 , v         = n
              }
-   where f = factorise n
+   where fs = factorise n
 
-factorsDict =  fromList $ map (\x-> (x, factors x)) [1..120000]
+factorsDict =  fromList $ zip [1..] . map factors $ [1..120000]
 
-abcHitFor c = if allOneExp fc
-              then []
-              else go [1..ceil]
-              --go [1..ceil]
-     where
-           fc = factorsDict ! c
-           ceil = let half = c `quot` 2 in if even c then half - 1 else half
+probSeq = map snd . filter (allOneExp.snd) . toList $ factorsDict
+
+abcHitFor c = if allOneExp fc then [] else go [1..ceil]
+     where fc = factorsDict ! c
+           ceil = c `quot` 2
            go [] = []
-           go (a:as) = if rad fa * rad fc > c
-                       then go as
-                       else if rad fa * rad fb * rad fc > c
-                            then go as
-                            else if (gcd1 fa fb && gcd1 fa fc && gcd1 fb fc)
-                                 then [c]
-                                 else go as
+           go (a:as)
+               | rac > c || rabc >= c  || not allGcds = go as
+               | otherwise = c : go as
                where b = c - a
                      fa = factorsDict ! a
                      fb = factorsDict ! b
+                     rac = rad fa * rad fc
+                     rabc = rad fa * rad fb * rad fc
+                     allGcds = gcd1 fa fb && gcd1 fa fc && gcd1 fb fc
 
 gcd1 fx fy = null (fseq fx `intersect` fseq fy)
 
 
-euler n = concatMap abcHitFor $ [2..n]
+euler n = concatMap abcHitFor [2..n]
+
+e = euler 120000
+
+p127 = sum e

90d4cc7cc97623e74b49c4b48f84ab652e82245d
wanglijun <juns1984@qq.com>
2016-08-18 18:32:23 +0800

    add mathematica solve

--- src/Euler/P203.hs
+++ src/Euler/P203.hs
@@ -1,2 +1,19 @@
 module Euler.P203 where
 
+import Math.NumberTheory.Primes
+import Data.List (nub,sort)
+
+binSeq = go [1]
+    where go xs = xs : go xs'
+             where xs' = 1 : zipWith (+) xs (tail xs) ++ [1]
+
+euler = sum                           .
+        filter (not.isSquareFactored) .
+        sort                          .
+        nub                           .
+        concat                        .
+        take 51                       $
+        binSeq
+
+isSquareFactored n = any (>1) es
+    where es = map snd . factorise $ n

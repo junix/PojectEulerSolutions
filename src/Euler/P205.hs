c4832897790f477d53881290a2aa2202ca4a5c7f
wanglijun <juns1984@qq.com>
2016-08-19 10:26:10 +0800

    add mathematica solve

--- src/Euler/P205.hs
+++ src/Euler/P205.hs
@@ -1,43 +1,21 @@
 module Euler.P205 where
-import System.Random
 import Data.Ratio
 import Data.List (groupBy,sort)
 import Data.Function (on)
-import Text.Printf(printf)
 
--- simulation method
-rseq :: Int -> Integer -> [Integer]
-rseq seed range = go . mkStdGen $ seed
-    where go gen = let (r,g) = randomR (1,range) gen
-                   in r : go g
+basicProbs n = map (\x -> (x,1%n)) [1..n]
 
-groupSeq seed range groupSize = go xs
-    where xs = rseq seed range
-          go xs = let (hs,ts) = splitAt groupSize xs
-                  in sum hs : go ts
+simplify = map merge . groupBy ((==) `on` fst) . sort
+    where merge xs = (x, p)
+              where x = fst . head    $ xs
+                    p = sum . map snd $ xs
 
-pyraSeq seed = groupSeq seed 4 9
-cubeSeq seed = groupSeq seed 6 6
+play 1 ps bs = ps
+play n ps bs = play (n-1) (simplify ps') bs
+    where ps' = [ (a+b, pa*pb) | (a, pa) <- ps, (b, pb) <- bs ]
 
-matchSeq seed n = length . filter (==True). take n $ zipWith (>) (pyraSeq seed) (cubeSeq seed)
+pyraProbs = let ps = basicProbs 4 in play 9 ps ps
+cubeProbs = let ps = basicProbs 6 in play 6 ps ps
 
-euler = matchSeq 1234 (10^7)
-
--- math solution
-baseProb n = map (\x -> (x,1%n)) [1..n]
-
-merge xs = (x, p)
-    where x = fst . head $ xs
-          p = sum . map snd $ xs
-
-uniq = map merge . groupBy ((==) `on` fst) . sort
-
-dice 1 ps bs = ps
-dice n ps bs = dice (n-1) ps' bs
-    where ps' = uniq [ (a+b, pa*pb) | (a, pa) <- ps, (b, pb) <- bs ]
-
-pyraProbs = dice 9 (baseProb 4) (baseProb 4)
-cubeProbs = dice 6 (baseProb 6) (baseProb 6)
-
-euler0 = print (fromRational prob)
-   where prob = sum [ pp*cp | (pv,pp) <- pyraProbs, (cv,cp) <- cubeProbs, pv > cv]
+euler = round . (*10^7) . fromRational . sum $ probs
+   where probs = [ pp*cp | (pv,pp) <- pyraProbs, (cv,cp) <- cubeProbs, pv > cv ]

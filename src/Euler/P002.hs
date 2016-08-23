09b5234b72c5a18aa58c55c11aa06289330e1d91
wanglijun <juns1984@qq.com>
2016-08-23 09:36:25 +0800

    add PE solution

--- src/Euler/P002.hs
+++ src/Euler/P002.hs
@@ -4,3 +4,7 @@ calc = sum . filter even . takeWhile (<=4000000) $ fibs
     where fibs = 1:2:fibs' 1 2
           fibs' r l = n : fibs' l n
             where n = l + r
+
+fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
+
+pe01 = sum . filter even . takeWhile (<=4000000) $ fibs
